d2adf12605278d00670f17c55219691df026ab18
erme07 <erik.medina07@gmail.com>
2024-10-31 09:28:39 -0300

    update

--- pre_entrega/script.py
+++ pre_entrega/script.py
@@ -1,6 +1,14 @@
 
 #Inventario de libros
 
+import os
+
+def limpiar_consola():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 inventario = [ #Contiene: Titulo - Autor - Categoria - Stock
   ["Fuego y Sangre","George R.R. Martin","Fantasia",30],
   ["El Hobbit","J.R.R. Tolkien","Fantasia",25],
@@ -10,9 +18,11 @@ inventario = [ #Contiene: Titulo - Autor - Categoria - Stock
   ["Los Crimenes de la Calle Morgue","Edgar Allan Poe","Policial",13],
   ["La Guerra de los Mundos","H.G. Wells","Ciencia Ficcion",24]
 ]
+
 opcion = None
 
 while opcion!=3:
+  limpiar_consola()
   while True:
     print("")
     print("-"*30)
@@ -25,20 +35,31 @@ while opcion!=3:
     opcion = input("Seleccione una opcion: ")
     if opcion.isdigit() or (opcion.startswith('-') and opcion[1:].isdigit()):
       opcion = int(opcion)
-      break
+      if(opcion < 1 or opcion > 3):
+        print(f"\n\"{opcion}\" no es una opcion valida, intente nuevamente")
+        input("\nPresione ENTER para continuar... ")
+        limpiar_consola()
+      else:
+        break
     else:
-      print(f"\"{opcion}\" no es una opcion valida, intente nuevamente")
+      print(f"\n\"{opcion}\" no es una opcion valida, intente nuevamente")
+      input("\nPresione ENTER para continuar... ")
+      limpiar_consola()
 
   if(opcion==1): #Ingreso de nuevo libro
+    limpiar_consola()
     titulo = input("\nIngrese el titulo: ")
     autor = input("\nIngrese el autor: ").title()
     while True:
+      limpiar_consola()
       print("\nCategorias: [Terror - Ciencia Ficcion - Romance - Fantasia - Policial]\n")
       categoria = input("Ingrese la categoria: ").title()
       if(categoria=="Terror" or categoria=="Ciencia Ficcion" or categoria=="Romance" or categoria=="Fantasia" or categoria=="Policial"):
         break
       print(f"\n\"{categoria}\" no es una categoria valida, intente nuevamente")
+      input("\nPresione ENTER para continuar... ")
     while True:
+      limpiar_consola()
       stock = input("\nIngrese la cantidad: ")
       if stock.isdigit() or (stock.startswith('-') and stock[1:].isdigit()):
         num = int(stock)
@@ -46,12 +67,16 @@ while opcion!=3:
           break
       else:
         print("\nError: La cantidad debe ser un numero entero mayor a cero.")
+        input("\nPresione ENTER para continuar... ")
     
     libro = [titulo,autor,categoria,stock]
     inventario.append(libro)
 
   elif(opcion==2): #Visualizacion de inventario
-    print("")
+    limpiar_consola()
+    print("-"*30)
+    print("INVENTARIO DE LIBROS")
+    print("-"*30)
     print("x","-"*50,"x","-"*30,"x","-"*15,"x","-"*6,"x")
     print("| {:<50} | {:<30} | {:<15} | {:<6} |".format("Titulo", "Autor", "Categoria", "Stock"))
     print("x","-"*50,"x","-"*30,"x","-"*15,"x","-"*6,"x")
@@ -60,4 +85,6 @@ while opcion!=3:
       print("| {:<50} | {:<30} | {:<15} | {:<6} |".format(inventario[num_libro][0], inventario[num_libro][1], inventario[num_libro][2], inventario[num_libro][3]))
       num_libro+=1
     print("x","-"*50,"x","-"*30,"x","-"*15,"x","-"*6,"x")
+    input("\nPresione ENTER para continuar... ")
+    limpiar_consola()
 
